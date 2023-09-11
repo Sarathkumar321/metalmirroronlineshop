0bcfae315325e1d267f2d047d0fb35b1ddee17e2
Lenovo <sarathkumarup123@gmail.com>
2023-09-11 20:30:55 +0530

    Add project files.

---
+++ metalmirroronlineshop/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace metalmirroronlineshop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
